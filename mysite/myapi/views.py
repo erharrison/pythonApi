36605da88689f4cc5a08d2265c7174f78eebfe3a
Emily <emilypbi@gmail.com>
2021-11-17 15:49:09 +0000

    Create Image model view.

--- mysite/myapi/views.py
+++ mysite/myapi/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import ImageSerializer
+from .models import Image
 
-# Create your views here.
+
+class ImageViewSet(viewsets.ModelViewSet):
+    queryset = Image.objects.all().order_by('likes')
+    serializer_class = ImageSerializer
